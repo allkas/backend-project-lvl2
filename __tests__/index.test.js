19033d7142fe53277e0ff77f39683c73fc016791
алкаш <all-kas@yandex.ru>
2020-03-03 12:19:00 +0300

    4 step

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -4,7 +4,7 @@ import genDiff from '../src/index.js';
 
 
 test('check json', () => {
-  const diff = genDiff('__tests__/__fixtures__/before.json', '__tests__/__fixtures__/after.json');
+  const diff = genDiff('__fixtures__/before.json', '__fixtures__/after.json');
   const expected = '{"- proxy":"123.234.53.22","- follow":false,"+ verbose":true,"+ timeout":20,"- timeout":50,"host":"hexlet.io"}';
   expect(diff).toBe(expected);
 });
