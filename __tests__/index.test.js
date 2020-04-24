f5ce04100f906bf39e23a0835bb600eef89bcf4a
алкаш <all-kas@yandex.ru>
2020-04-24 16:05:24 +0300

    5 step

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,6 +1,6 @@
 import fs from 'fs';
 import path from 'path';
-import genDiff from '..';
+import genDiff from '../src/index.js';
 
 const getPath = (filename) => path.join('__fixtures__', filename);
 
